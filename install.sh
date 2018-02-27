c922b5ec04d59c17863de8463bd9cdb846ef039c
Vladimirs Nordholm <v@vladde.net>
2018-02-27 20:19:39 +0100

    [install] tweaks :)

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo "> started"
+echo "> linking configuration files by @vladdeSV"
 echo
 for file in .*; do
     if [ -f $file ]; then
-        echo "> copying $file";
+        echo "> linking $file";
         if [ -f "${HOME}/$file" ]; then
-            echo "  conflicting $file found. renaming old file"
+            echo "!> conflicting $file found. renaming old file"
             mv "${HOME}/$file" "${HOME}/$file.old"
         fi
         
-        cp $file "${HOME}/$file"
+        ln -s "$(pwd)/$file" "${HOME}/$file"
     fi
 done
 
 echo
-echo "> running .bash_profile"
+echo "> updating .bash_profile"
 source ~/.bash_profile
 echo "> done"
