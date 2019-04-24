b2d4fed0b458957d0992beb3d28b4dd9bdfba89f
Vladimirs Nordholm <v@vladde.net>
2019-04-24 20:43:24 +0200

    [fish] add git

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,11 @@ set -x LANG en_GB.utf8
 
 alias vim=nvim
 alias :q=exit
+
+# git
+alias gs='git status'
+alias gp='git pull'
+alias gpush='git push'
+alias gd='git diff -w'
+alias gds='gd --staged'
+alias gco='git checkout'
