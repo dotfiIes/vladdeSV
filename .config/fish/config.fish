ba2388657ee973c3b96dda3d9ca258c49de42714
Vladimirs Nordholm <v@vladde.net>
2019-04-24 19:16:56 +0200

    [fish] add config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+set -U fish_prompt_pwd_dir_length 0
+set -x LANG en_GB.utf8
+
+alias vim=nvim
+alias :q=exit
